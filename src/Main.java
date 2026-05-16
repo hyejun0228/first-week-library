7220fe03b70a32fb43c4826399810be6494ff6b1
hyejun <hyejunkkim228@gmail.com>
2026-05-16 22:50:40 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
+        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
+        System.out.printf("=================================\n");
+        System.out.printf("     도서관에 오신것을 환영합니다\n");
+        System.out.printf("=================================\n");
+        System.out.printf("1. 회원 등록\n");
+        System.out.printf("2. 도서 등록 \n");
+        System.out.printf("3. 전체 도서 조회 \n");
+        System.out.printf("4. 전체 회원 조회 \n");
+        System.out.printf("5. 도서 대여 \n");
+        System.out.printf("6. 도서 반납 \n");
+        System.out.printf("7. 종료 \n");
+
+
+    }
+}
